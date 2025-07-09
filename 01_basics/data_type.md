983bd43171cdc069bf9460553f0ea72b7aacd6ae
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-07-10 02:14:09 +0530

    V 6 and 7 Covered

---
+++ 01_basics/data_type.md
@@ -0,0 +1,24 @@
+# Object Types / Data Types
+
+- Number : 1234, 3.134, 3+4j, 0b11, Decimal(), Fraction()
+- String : 'hello' , "Bob's", b'a\x01x'
+- List : [1 ,[2,'Three'],4.5] , list(range(10))
+- Tuple : (1,'some',4,'U') , tuple('name'), namedtuple
+- Dictionary : {'food':'nice'} , dict(hours=10)
+
+- Set : set('abc'),{'a','b','c'}
+
+- File : open('file.txt')
+
+- Boolean : True,False
+- None : None
+- Function, modules,classes
+- Advance : Decorators, Generators, Iterators, MetaProgramming
+
+# Note Data type is always of the value or object not of varaible
+
+like if we have
+a= 3 3 is of number type a is not of number type
+a= "hello" hello is of string type a is not of string type
+
+Number and strings are not garbage collected immeditately in python other data types are they are garbage collected after some time
--- 01_basics/mutable_immutable.py
+++ 01_basics/mutable_immutable.py
@@ -23,3 +23,53 @@ username="Aaka"           # here Aakash is not updated it is present there only 
 x = 12       # reference of 12 is created and x is pointing at that
 y = x         # y is also pointing at same reference of x that is 12
 x = 15        # now reference of 15 is created and x is pointing at that and y is still pointing at 12 only
+
+
+# // List is Mutable In first example refereing to same memory reference 
+# // in Second when we are doing  p2=[1,2,3] then creating new memory reference
+# >>> l1=[1,2,3]
+# >>> l2=l1
+# >>> l1
+# [1, 2, 3]
+# >>> l2
+# [1, 2, 3]
+# >>> l1[0]=44
+# >>> l1
+# [44, 2, 3]
+# >>> l2
+# [44, 2, 3]
+# >>> p1=[1,2,3]
+# >>> p2=p1
+# >>> p2=[1,2,3]
+# >>> p1[0]=55
+# >>> p1
+# [55, 2, 3]
+# >>> p2
+# [1, 2, 3]
+# >>> 
+# in easy what we can do is
+# This gives us copy so different memory reference for both
+# h1=[1,2,3]
+# h2=h1[:]
+# OR
+# import copy
+# h2=copy.copy(h1) // Single Level
+# h2=copy.deepcopy(h1) // Nested Levels too
+
+
+# >>> n = [1,2,3]
+# ... m =n 
+# 
+# >>> m
+# [1, 2, 3]
+# >>> n
+# [1, 2, 3]
+# >>> m ==n     // Checks equality
+# True
+# >>> m is n    // Checks Memory Reference
+# True
+# >>> n = [1,2,3]
+# >>> m==n
+# True
+# >>> m is n
+# False
